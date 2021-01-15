5b4d97c8f83eb4a89939dae0674e8118d6265cd5
carolina <carolina.beatriz.cardenas@gmail.com>
2021-01-15 12:41:45 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//variables y contantes
+const nombre = 'carolina'
+const apellido = 'cardenas'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let valorDado = 5;
+
+console .log ( nombre, apellido, valorDado )
