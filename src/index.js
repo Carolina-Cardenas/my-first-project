ed8aaec5db8eaffe3ee1443ac989398d58e510be
carolina <carolina.beatriz.cardenas@gmail.com>
2021-02-24 20:36:55 +0100

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 
-//variables y contantes
-const nombre = 'carolina'
-const apellido = 'cardenas'
 
-let valorDado = 5;
+const apikey = 'v7nbUloGro9JzfD41hKV7QbhpWgFw7bd';
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apikey }`);
 
-console .log ( nombre, apellido, valorDado )
+peticion
+    .then(resp => resp.json() )
+    .then( ({data} )=> {
+        const {url} = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+    })
+    .catch(console.warn)
+
